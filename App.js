10f9e8f2735fbad609b6b0ddf6dcd74141a504d3
usamaltaf <ualtaf238@gmail.com>
2022-07-23 21:44:40 +0500

    camera working

--- App.js
+++ App.js
@@ -1,85 +1,116 @@
-import React from "react";
-import {
-  Text,
-  Link,
-  HStack,
-  Center,
-  Heading,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme,
-  VStack,
-  Box,
-} from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
-import { Platform } from "react-native";
-
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
-
-// extend the theme
-export const theme = extendTheme({ config });
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View,Button,Image, TouchableOpacity, SafeAreaView } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as MediaLibary from 'expo-media-library';
+import {StatusBar } from 'native-base';
 
 export default function App() {
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [haslibraryPermission, sethaslibraryPermission] = useState(null)
+  const [type, setType] = useState('back');
+const [pic, setpic] = useState('')
+  useEffect(() => {
+    (async () => {
+      const { Camerastatus } = await Camera.requestCameraPermissionsAsync();
+      const { librarystatus } = await MediaLibary.requestPermissionsAsync();
+    
+      setHasCameraPermission(Camerastatus === 'granted');
+      sethaslibraryPermission(librarystatus === 'granted'); 
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined || haslibraryPermission===undefined) {
+    return <Text>Requesting Permissions</Text>;
+  }
+ else if (hasCameraPermission===null) {
+    return <Text>No access to camera</Text>;
+  }
+
+  let takePicture =async () => {
+    let options = {
+      quality: 0,
+      base64: true,
+      exif: false,
+      isImageMirror:false
+    };
+    let newpic = await cameraRef.current.takePictureAsync(options);
+    console.log('being save',newpic);
+    setpic(newpic);
+  }
+  if (pic) {
+    let savePhoto = () => {
+      MediaLibary.saveToLibraryAsync(pic.uri).then((res) => {
+        console.log('being save',res);
+        setpic(undefined);
+      })
+    }
+    return (
+      <SafeAreaView style={{ flex: 1,alignSelf:'stretch' }}>
+       
+       <Image style={{ height:500,width:'100%' }} source={{ uri: "data:image/jpg;base64," + pic.base64 }} />
+    {
+          pic == undefined ?
+            <Button title="Discard" onPress={()=>setpic(undefined)} /> :
+            <Button title="save Pic"  onPress={()=>savePhoto()}/>
+    } 
+      </SafeAreaView>
+    )
+  }
   return (
-    <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
+    <SafeAreaView style={styles.camera}>
+      <StatusBar />
+      <Camera style={styles.camera}
+        ref={cameraRef}
+        type={type}
+        autoFocus={true}
+        ratio={'16:9'}
+        useCamera2Api={false}
       >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
-    </NativeBaseProvider>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(type === 'back' ? 'front' : 'back');
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              takePicture();
+            }}>
+            <Text style={styles.text}> Take Pic </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+      </SafeAreaView>
   );
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+const styles = StyleSheet.create({
+
+  camera: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  buttonContainer: {
+    flex:1,
+    flexDirection: 'row',
+    margin: 20,
+   
+  },
+  button: {
+    flex: 1,
+    borderRadius: 10,
+    padding:10,
+    backgroundColor:'#ffff',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: '#000000',
+  },
+});
