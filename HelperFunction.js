34c1e3079278fb11366c48a18c1d35dc05ac4c7a
Usama Altaf <ualtaf238@gmail.com>
2023-02-12 13:24:58 +0500

    added env

--- HelperFunction.js
+++ HelperFunction.js
@@ -1,11 +1,12 @@
 
-import {API_KEY1,API_KEY2,API_KEY3,API_KEY4} from "@env"
+import {API_KEY1,API_KEY2,API_KEY3,API_KEY4} from "./env"
 //this endpoint will tell Google to use the Vision API. We are passing in our key as well.
 
 const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY1}`;
 const API_URL2 = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY2}`;
 const API_URL3 = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY3}`;
 const API_URL4 = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY4}`;
+
 
 function generateBody(image) {
   const body = {
@@ -25,9 +26,7 @@ function generateBody(image) {
   };
   return body;
 }
-
-async function callGoogleVisionAsync(image) {
-  const body = generateBody(image); //pass in our image for the payload
+async function fetching(body,url){
   const response = await fetch(API_URL, {
     method: 'POST',
     headers: {
@@ -37,11 +36,20 @@ async function callGoogleVisionAsync(image) {
     body: JSON.stringify(body),
   });
   const result = await response.json();
-  console.log('res',result.error.code);
-  
-  const detectedText = result.responses[0]?.fullTextAnnotation;
-  return detectedText
-  ? detectedText
-  : { text: "" };
+  return result;
+}
+async function callGoogleVisionAsync(image) {
+
+ const body = generateBody(image); //pass in our image for the payload
+ const data=await fetching(body,API_URL).then((res)=>{
+  console.log(res);
+  return res;
+ }).catch((err)=>{
+  return err;
+ })
+ const detectedText = data.responses[0]?.fullTextAnnotation;
+ return detectedText
+ ? detectedText
+ : { text: "" };
 }
 export default callGoogleVisionAsync;
